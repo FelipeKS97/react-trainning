cda2e7c35558543c0dbd8da97dcbe37884335113
Felipe Cavalcanti <felipecavalcanti100@gmail.com>
2017-08-30 16:08:13 -0300

    Adicionando redux ao component, faltam ajustes

--- ex/counterReducer.js
+++ ex/counterReducer.js
@@ -1,13 +1,13 @@
-const INITIAL_STATE = { step:1, number:0 }
+const INITIAL_STATE = { step: 1, number:0 }
 
-export default function (state, action) {
+export default function (state = INITIAL_STATE, action) {
     switch(action.type) {
         case 'INC':
-            return {...this.state, number: state.step + state.number}
+            return {...state, number: state.step + state.number}
         case 'DEC':
-            return {...this.state, number: state.step - state.number}
+            return {...state, number: state.step - state.number}
         case 'STEP_CHANGED':
-            return {...this.state, step: action.payload}
+            return {...state, step: +action.payload}
         default:
             return state
     }
